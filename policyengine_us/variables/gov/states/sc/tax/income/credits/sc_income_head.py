4cc8767bfa9975871a716fa98ec09a861c76134b
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-05-24 15:50:50 +0000

    define sc income spouse
    Co-authored-by: Pinyan <pxu12@users.noreply.github.com>
    Co-authored-by: PavelMakarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/sc/tax/income/credits/sc_income_head.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/sc_income_head.py
@@ -8,3 +8,9 @@ class sc_income_head(Variable):
     defined_for = StateCode.SC
     unit = USD
     definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        head = person("is_tax_unit_head", period)
+        income = tax_unit("sc_taxable_income", period)
+        return tax_unit.sum(head * income)
--- policyengine_us/variables/gov/states/sc/tax/income/credits/sc_income_spouse.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/sc_income_spouse.py
@@ -8,3 +8,9 @@ class sc_income_spouse(Variable):
     defined_for = StateCode.SC
     unit = USD
     definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        spouse = person("is_tax_unit_spouse", period)
+        income = tax_unit("sc_taxable_income", period)
+        return tax_unit.sum(spouse * income)
