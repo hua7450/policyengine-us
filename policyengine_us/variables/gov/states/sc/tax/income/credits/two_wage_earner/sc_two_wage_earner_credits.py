2f2bd7f1a6348a3fe10d99e7b9f0ba8daea0089c
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-06-13 18:46:03 +0000

    South Carolina Two wage earner credit Fixes #2307
    Co-authored-by: Pinyan <pxu12@users.noreply.github.com>

--- policyengine_us/variables/gov/states/sc/tax/income/credits/two_wage_earner/sc_two_wage_earner_credits.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/two_wage_earner/sc_two_wage_earner_credits.py
@@ -9,7 +9,7 @@ class sc_two_wage_earner_credits(Variable):
     defined_for = StateCode.SC
     unit = USD
     definition_period = YEAR
-    reference = "https://dor.sc.gov/forms-site/Forms/SC1040TT_2022.pdf"
+    reference = "https://dor.sc.gov/forms-site/Forms/SC1040TT_2021.pdf"
 
     def formula(tax_unit, period, parameters):
         # First get their filing status.
@@ -19,20 +19,6 @@ class sc_two_wage_earner_credits(Variable):
         p = parameters(
             period
         ).gov.states.sc.tax.income.credits.two_wage_earner.rates
-
-        # Get the individual filer's income and ira_dudction
-        income_head = tax_unit("sc_income_head", period)
-        ira_deduction_head = tax_unit("sc_ira_deduction_head", period)
-
-        # head's income - ira_dudction
-        head_eligible = income_head - ira_deduction_head
-
-        # Get the spouse income and ira_dudction
-        income_spouse = tax_unit("sc_income_spouse", period)
-        ira_deduction_spouse = tax_unit("sc_ira_deduction_spouse", period)
-
-        # spouse's income - ira_dudction
-        spouse_eligible = income_spouse - ira_deduction_spouse
 
         # Determine the filing status
         joint = filing_status == filing_status.possible_values.JOINT
