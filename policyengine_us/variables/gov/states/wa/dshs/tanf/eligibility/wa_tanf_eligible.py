7e50dc7cbf883a9b29591110a9c72bffadb83543
Ziming <ziming@policyengine.org>
2026-03-23 14:20:51 -0400

    fix

--- policyengine_us/variables/gov/states/wa/dshs/tanf/eligibility/wa_tanf_eligible.py
+++ policyengine_us/variables/gov/states/wa/dshs/tanf/eligibility/wa_tanf_eligible.py
@@ -11,18 +11,7 @@ class wa_tanf_eligible(Variable):
 
     def formula(spm_unit, period, parameters):
         demographic_eligible = spm_unit("is_demographic_tanf_eligible", period)
-
-        # Must have at least one U.S. citizen or qualified immigrant
-        # Per WAC 388-424-0001, follows federal immigration eligibility
-        has_citizen = spm_unit.any(person("is_citizen_or_legal_immigrant", period))
-
-        # Must meet income eligibility (gross earned income < limit)
-        # Per WAC 388-478-0035
+        has_citizen = add(spm_unit, period, ["is_citizen_or_legal_immigrant"]) > 0
         income_eligible = spm_unit("wa_tanf_income_eligible", period)
-
-        # Must meet resource eligibility (countable resources <= $6,000)
-        # Per WAC 388-470-0045
         resources_eligible = spm_unit("wa_tanf_resources_eligible", period.this_year)
-
-        # All requirements must be met
         return demographic_eligible & has_citizen & income_eligible & resources_eligible
--- policyengine_us/variables/gov/states/wa/dshs/tanf/wa_tanf.py
+++ policyengine_us/variables/gov/states/wa/dshs/tanf/wa_tanf.py
@@ -14,15 +14,8 @@ class wa_tanf(Variable):
     defined_for = "wa_tanf_eligible"
 
     def formula(spm_unit, period, parameters):
-        # Get payment standard based on assistance unit size
-        # (capped at maximum_family_size, typically 10)
         payment_standard = spm_unit("wa_tanf_payment_standard", period)
-
-        # Get countable income (after all disregards and deductions)
-        countable_income = spm_unit("wa_tanf_countable_income", period)
-
-        # Calculate benefit per WAC 388-450-0165:
-        # "The department calculates the amount of your cash assistance
-        # by subtracting your AU's countable income from the applicable
-        # payment standard."
+        countable_income = max_(
+            spm_unit("wa_tanf_countable_income", period), 0
+        )
         return max_(payment_standard - countable_income, 0)
