2f83acaddc7968169e0945cb13722d9ca8383416
Max Ghenis <mghenis@gmail.com>
2026-03-29 14:19:42 -0400

    Format Wisconsin state tax overrides

--- policyengine_us/variables/gov/states/tax/income/state_income_tax.py
+++ policyengine_us/variables/gov/states/tax/income/state_income_tax.py
@@ -29,4 +29,8 @@ class state_income_tax(Variable):
             refundable_credits = add(tax_unit, period, ["state_refundable_credits"])
             default_tax = income_tax_before_refundable_credits - refundable_credits
             state_code = tax_unit.household("state_code", period)
-            return where(state_code == StateCode.WI, tax_unit("wi_income_tax", period), default_tax)
+            return where(
+                state_code == StateCode.WI,
+                tax_unit("wi_income_tax", period),
+                default_tax,
+            )
--- policyengine_us/variables/household/income/household/household_state_income_tax.py
+++ policyengine_us/variables/household/income/household/household_state_income_tax.py
@@ -26,4 +26,8 @@ class household_state_income_tax(Variable):
                 ["state_income_tax_before_refundable_credits"],
             ) - add(tax_unit, period, ["state_refundable_credits"])
             state_code = tax_unit.household("state_code", period)
-            return where(state_code == StateCode.WI, tax_unit("wi_income_tax", period), default_tax)
+            return where(
+                state_code == StateCode.WI,
+                tax_unit("wi_income_tax", period),
+                default_tax,
+            )
