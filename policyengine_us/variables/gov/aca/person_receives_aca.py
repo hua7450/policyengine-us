87bd23eed86a2e0c21136caeacbf27a0f3f6ba92
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-04-02 14:18:19 -0400

    Fix ACA receipt semantics

--- policyengine_us/variables/gov/aca/person_receives_aca.py
+++ policyengine_us/variables/gov/aca/person_receives_aca.py
@@ -7,11 +7,11 @@ class person_receives_aca(Variable):
     label = "Person receives ACA"
     documentation = (
         "Whether a person is eligible for ACA premium tax credits and is in a "
-        "tax unit with positive assigned ACA premium tax credit."
+        "tax unit that takes up ACA coverage."
     )
     definition_period = YEAR
 
     def formula(person, period, parameters):
-        return (person.tax_unit("assigned_aca_ptc", period) > 0) & person(
+        return person.tax_unit("takes_up_aca_if_eligible", period) & person(
             "is_aca_ptc_eligible", period
         )
