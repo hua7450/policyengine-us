cfa610834ca4512cdfdc822046898d754d85cfb5
Max Ghenis <mghenis@gmail.com>
2026-04-10 14:55:01 -0400

    Handle SSTB income in self-employment deductions

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/above_the_line_deductions/loss_ald.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/above_the_line_deductions/loss_ald.py
@@ -14,7 +14,7 @@ class loss_ald(Variable):
         filing_status = tax_unit("filing_status", period)
         max_loss = parameters(period).gov.irs.ald.loss.max[filing_status]
         person = tax_unit.members
-        indiv_se_loss = max_(0, -person("self_employment_income", period))
+        indiv_se_loss = max_(0, -person("total_self_employment_income", period))
         self_employment_loss = tax_unit.sum(indiv_se_loss)
         limited_capital_loss = tax_unit("limited_capital_loss", period)
         return min_(max_loss, self_employment_loss + limited_capital_loss)
